c6a8330a1578feaef9b3ac3a1f83a67ef215a3e6
akhilsaisrikar <136218204+akhilsaisrikar@users.noreply.github.com>
2025-11-07 08:39:24 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,72 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height();
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+
+    $(window).on('scroll', function(){
+      function isScrollIntoView(elem, index) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(window).height()*.5;
+        if(elemBottom <= docViewBottom && elemTop >= docViewTop) {
+          $(elem).addClass('active');
+        }
+        if(!(elemBottom <= docViewBottom)) {
+          $(elem).removeClass('active');
+        }
+        var MainTimelineContainer = $('#vertical-scrollable-timeline')[0];
+        var MainTimelineContainerBottom = MainTimelineContainer.getBoundingClientRect().bottom - $(window).height()*.5;
+        $(MainTimelineContainer).find('.inner').css('height',MainTimelineContainerBottom+'px');
+      }
+      var timeline = $('#vertical-scrollable-timeline li');
+      Array.from(timeline).forEach(isScrollIntoView);
+    });
+  
+  })(window.jQuery);
+  // custom.js: Enhanced Course Marquee Stop/Resume
+
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollingLines = document.querySelectorAll('.course-scrolling-line');
+    
+    scrollingLines.forEach(line => {
+        const wrapper = line.querySelector('.course-list-wrapper');
+        
+        // Pause animation when mouse enters the line (covers both line and card hover)
+        line.addEventListener('mouseenter', () => {
+            wrapper.style.animationPlayState = 'paused';
+        });
+
+        // Resume animation when mouse leaves the line
+        line.addEventListener('mouseleave', () => {
+            wrapper.style.animationPlayState = 'running';
+        });
+    });
+});
+
+
